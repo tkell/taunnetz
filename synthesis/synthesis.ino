d4a96ad17d8dd1bbd4c470e0eb536ac012a744eb
tkell <thor.kell@mail.mcgill.ca>
2013-03-09 20:06:10 -0500

    Got it:  don't multiply in updateAudio

--- synthesis/synthesis.ino
+++ synthesis/synthesis.ino
@@ -7,8 +7,7 @@
 #include <EventDelay.h>
 #include <mozzi_midi.h>
 #include <fixedMath.h> // for Q16n16 fixed-point fractional number type
-#include <tables/cos8192_int8.h>
-#include <tables/sin8192_int8.h>
+#include <tables/sin2048_int8.h>
 
 #define CONTROL_RATE 128
 #define NUMBER_OSCS 8
@@ -16,21 +15,17 @@
 byte newButtons[NUMBER_OSCS];
 byte oldButtons[NUMBER_OSCS];
 
-// Amazingly shitty need to declare every os by hand
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc1(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc2(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc3(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc4(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc5(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc6(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc7(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc8(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc9(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc10(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc11(SIN8192_DATA);
-Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> osc12(SIN8192_DATA);
+// Amazingly shitty need to declare every osc by hand
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc1(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc2(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc3(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc4(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc5(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc6(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc7(SIN2048_DATA);
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc8(SIN2048_DATA);
 
-Oscil<SIN8192_NUM_CELLS, AUDIO_RATE> *oscs[NUMBER_OSCS] = {
+Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> *oscs[NUMBER_OSCS] = {
     &osc1, &osc2, &osc3, &osc4, &osc5, &osc6, &osc7, &osc8
   };
 
@@ -40,18 +35,15 @@ Q16n16 frequency;
 
 void setup() {
   startMozzi(CONTROL_RATE);
-  Serial.begin(9600);
   eventDelay.set(1000); // 1 second countdown, within resolution of CONTROL_RATE
-  
   
   
   for (int i = 0; i < NUMBER_OSCS; i++) {
     newButtons[i] = 0;
     oldButtons[i] = 0;
     
-    // Trying to automate the frequency setting
+    // Trying to automate the frequency setting - would be nice to get this in semitones,..
     frequency = Q16n16_mtof(Q16n0_to_Q16n16(i + 71));
-    Serial.println(frequency);
     oscs[i]->setFreq_Q16n16(frequency);
    
   }
