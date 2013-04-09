862fa3a2f28c3ab77a7cd222d4060726a8e104f7
tkell <thor.kell@mail.mcgill.ca>
2013-04-08 23:13:29 -0400

    Adding pd patches and ripping things out for offline audio

--- test_capsense/firmware/firmware.ino
+++ test_capsense/firmware/firmware.ino
@@ -1,43 +1,13 @@
 // Cypress touch code via Joe, with deep thanks to Av, Ian, and Hackon for helping me with harwdware
+// This is the non-Mozzi version:  it sends serial data to some sort of synthesis program
+
 
 #include <Wire.h>
-
-#include <MozziGuts.h>
-#include <Oscil.h>
-#include <mozzi_midi.h>
-#include <fixedMath.h> // for Q16n16 fixed-point fractional number type
-#include <tables/triangle_warm8192_int8.h>
-
-// Synthesis code
-#define CONTROL_RATE 64 // 64 seems better than 128, 32 does not work
-#define NUMBER_OSCS 6 // cold change this, really
-#define NUMBER_CONDITIONS 4 // 4 works well.  More than 16 ruins my day.  May cut this totally?
-#define NOISE_THRESH 0x96  // 0x28 is factory default, 0x50 was working well, 0x72 was working great, 0xAA felt a little slow
+#define NUMBER_CONDITIONS 2 // 4 works well.  More than 16 ruins my day.  May cut this totally?
+#define NOISE_THRESH 0x72  // 0x28 is factory default, 0x50 was working well, 0x72 was working great, 0xAA felt a little slow
 #define NUMBER_CHIPS 6
 
-byte newOscs[NUMBER_OSCS];
-byte conditionData[NUMBER_CHIPS][NUMBER_CONDITIONS];
-
-// Amazingly shitty need to declare every osc by hand
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc1(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc2(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc3(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc4(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc5(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc6(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc7(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc8(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc9(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc10(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc11(TRIANGLE_WARM8192_DATA);
-Oscil <TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> osc12(TRIANGLE_WARM8192_DATA);
-
-Oscil<TRIANGLE_WARM8192_NUM_CELLS, AUDIO_RATE> *oscs[NUMBER_OSCS] = {
-    &osc1, &osc2, &osc3, &osc4, &osc5, &osc6
-  };
-
-Q16n16 frequency;
-int pitchArray[8];
+int pitchArray[NUMBER_CHIPS][8];
 
 // Touch code
 int xres1 = 2;  
@@ -46,6 +16,7 @@ int xres3 = 4;
 int xres4 = 5;  
 int xres5 = 6;  
 int xres6 = 7;
+byte conditionData[NUMBER_CHIPS][NUMBER_CONDITIONS];
 
 // I2C adresses
 #define I2C_ADDR0 0x00
@@ -210,105 +181,22 @@ void setup() {
     }  
   }
   
-  //Serial.println("Finished touch setup");
-  for (int i = 0; i < NUMBER_OSCS; i++) {
-    newOscs[i] = 0;
-  }
-  pitchArray = {57, 61, 65, 57, 61, 65, 64, 68};
-
-  delay(250);
-  //Serial.println("Finished Mozzi setup");
-  startMozzi(CONTROL_RATE);
+  // Initialize the pitch array here
+  
 }
+
 
 void loop() {
- audioHook();
-}
-
-
-int playNotes(byte touchData, int oscIndex, int frequencies[]) {
-  byte mask;
-  int freqIndex = 0;
-  
-  for (mask = 00000001; mask > 0; mask <<= 1) {
-    if (oscIndex >= NUMBER_OSCS) { 
-      break;
-    }
-    if (touchData & mask) {
-      frequency = Q16n16_mtof(Q16n0_to_Q16n16(frequencies[freqIndex]));
-      oscs[oscIndex]->setFreq_Q16n16(frequency);  
-      newOscs[oscIndex] = 1;
-      oscIndex++;
-    }
-    freqIndex++;
-  }
-  
-  return oscIndex;
-}
-
-void updateWombatControl(){
-  // put changing controls in here
-}
-
-void updateControl() {
   byte touchData = 0;
-  int oscIndex = 0; 
-
   // For 6 chips
-  //touchData = readTouch(I2C_ADDR0); // get the touch values from 1 x CY8C201xx chips - GP0 are the higher bits, GP1 the lower
-  //touchData = conditionTouchData(touchData, 0);
-  //touchData = 7;
-  //Serial.println(touchData, BIN);
-  // So this is GP0:  0, 1, 2, 3 - GP1:  0, 1, 2, 3
-  // I am re-writing based on proximity, so EACH of these will be different.  Sorry.
-  // A-C#-F, A-C#-F, E-Ab
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-  
-  //touchData = readTouch(I2C_ADDR1);
-  //touchData = conditionTouchData(touchData, 1);
-  //Serial.println(touchData, BIN);
-  //pitchArray = {60, 64, 68, 60, 59, 63, 67, 59}; // C, E-Ab-C, B-Eb-G, B    
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-  
-  //touchData = readTouch(I2C_ADDR2);
-  //touchData = conditionTouchData(touchData, 2);
-  //Serial.println(touchData, BIN);
-  //pitchArray = {63, 67, 66, 58, 62, 66, 58, 62};  // Eb-G, F#-Bb-D, F#-Bb-D  
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-   
-  //touchData = readTouch(I2C_ADDR3);
-  //touchData = conditionTouchData(touchData, 3);
-  //Serial.println(touchData, BIN);
-  //pitchArray = {69, 73, 77, 69, 73, 77, 76, 80};  //  A-C#-F, A-C#-F, E-Ab    
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-
-  //touchData = readTouch(I2C_ADDR4);
-  //touchData = conditionTouchData(touchData, 4);
-  //Serial.println(touchData, BIN);
-  //pitchArray = {72, 76, 80, 72, 71, 75, 79, 71};  // C, E-Ab-C, B-Eb-G, B    
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-
-  //touchData = readTouch(I2C_ADDR5);
-  //touchData = conditionTouchData(touchData, 5);
-  //Serial.println(touchData, BIN);
-  //pitchArray = {75, 79, 78, 70, 74, 78, 70, 74};  // Eb-G, F#-Bb-D, F#-Bb-D  
-  //oscIndex = playNotes(touchData, oscIndex, pitchArray);
-
-  // Turn off any unused oscillators
-  for (oscIndex; oscIndex < NUMBER_OSCS; oscIndex++) {
-    newOscs[oscIndex] = 0;
+  for (int i = 0; i < NUMBER_CHIPS; i++) {
+    touchData = readTouch(i); // get the touch values from 1 x CY8C201xx chips - GP0 are the higher bits, GP1 the lower
+    touchData = conditionTouchData(touchData, i);
+    // mask and send the pitch array midi numbers here
+    Serial.println(touchData, BIN);
+    //Serial.write(i);
+    //Serial.write(72);
   }
-}
-
-int updateAudio(){
-  int asig = 0;
-  for (int i = 0; i < NUMBER_OSCS; i++) {
-    if (newOscs[i] != 0) {
-      asig = asig + oscs[i]->next();
-    }
-  }
-  //  >> 3 works for 1-3 oscs.  Will need to solve this later
-  return asig >> 3;
 }
 
 
